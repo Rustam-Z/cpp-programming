59fdd9aa2a39f674632257c1e002bcd4eac6bb80
Rustam-Z <zokirovrustam202@gmail.com>
2023-04-18 01:39:31 +0500

    Updated bank management system

--- Bank management system/main.cpp
+++ Bank management system/main.cpp
@@ -1,6 +1,7 @@
 #include "Header.h"
 
-int main() {	
+int main()
+{
 	srand(time(0));
 	::available = rand() % 200000;
 	MainMenu();
@@ -10,12 +11,14 @@ int main() {
 	return 0;
 }
 
-void MainMenu() {
+void MainMenu()
+{
 	system("color 9E");
 	char num;
 	char choice;
-	do {
-		
+	do
+	{
+
 		bool checked = false;
 		string checkName, checkPassword;
 		accounts data;
@@ -37,48 +40,56 @@ void MainMenu() {
 			cout << "\n          Please enter your password:";
 			cout << "\n          ";
 			cin >> checkPassword;
-			for (int i = 0;i<::increaments; i++) {
-				
-				if (oldData[i].name == checkName && oldData[i].password == checkPassword) {
+			for (int i = 0; i < ::increaments; i++)
+			{
+
+				if (oldData[i].name == checkName && oldData[i].password == checkPassword)
+				{
 					::accountNum = i;
 					::balance = oldData[i].balance;
 					::borrow = oldData[i].borrowed;
 					checked = true;
 				}
-
 			}
-			if (checked) {
-				do {
+			if (checked)
+			{
+				do
+				{
 					system("CLS");
-					cout << "\n          Hello "<<oldData[::accountNum].name << endl;
+					cout << "\n          Hello " << oldData[::accountNum].name << endl;
 					cout << "\n          What would you like to do?" << endl;
 					cout << "\n          1.Actions with card" << endl;
 					cout << "\n          2.Check balance" << endl;
 					cout << "\n          0.Exit" << endl;
 					num = _getch();
-					switch (num) {
+					switch (num)
+					{
 					case 48:
 						break;
-					case 49: Menu(oldData);
+					case 49:
+						Menu(oldData);
 						break;
-					case 50: CheckBalance();
+					case 50:
+						CheckBalance();
 						break;
 					default:
-						cout << "\n          Error! Section not found. Please try one more time." << endl << endl;
+						cout << "\n          Error! Section not found. Please try one more time." << endl
+							 << endl;
 						system("pause");
 						break;
 					}
 
 				} while (num != '0');
 			}
-			else cout << "\n          Wrong name or password\n";
+			else
+				cout << "\n          Wrong name or password\n";
 			break;
 		case 50:
 			system("CLS");
 			cout << "\n          Input your name without space";
 			cout << "\n          ";
 			cin >> data.name;
-			cout << "\n          Input yuor password without space";
+			cout << "\n          Input your password without space";
 			cout << "\n          ";
 			cin >> data.password;
 			data.balance = 0;
@@ -88,19 +99,11 @@ void MainMenu() {
 		case 51:
 			break;
 		default:
-			cout << "\n          There is no such ssection";
+			cout << "\n          There is no such section";
 			cout << "\n          Please try one more time";
 			break;
 		}
 	} while (choice != 51);
 
-	
-
-
-	
 	cout << "\n          ";
 }
-
-
-
-
