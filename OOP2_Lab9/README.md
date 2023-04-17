00af6fa51a90c9b3aaa3edccfbc902bcd9b1c72f
Rustam-Z <zokirovrustam202@gmail.com>
2023-04-18 01:33:02 +0500

    OOP2 Lab9 updated

---
+++ OOP2_Lab9/README.md
@@ -0,0 +1,13 @@
+# Practical Lab Assignment - File Handling
+
+1. Create two file “one.txt” and “two.txt” which contains first 10 even numbers and first ten multiples of 5 respectively; Read the two files, find the sum of all the number of these two files and store it in the variable TOTAL. Write this value in third file named “total.txt”.
+
+2. A  file contains a  list of telephone numbers in the following form:
+    ```
+    John    23456
+    Ahmed	9876
+    ...     ...
+    ```
+The names contain only one word and the names and telephone numbers are separated by white spaces. Write a program to read the file and output the list in the two columns.
+
+3. Enter 20 numbers in a file named “Numbers.txt”. Ask user to enter any number and search if it exists in the file or not.
--- OOP2_Lab9/main.cpp
+++ OOP2_Lab9/main.cpp
@@ -1,4 +1,4 @@
-// All three programs are written here 
+// All three programs are written here
 // A menu driven program which allows to use all programs at the same time
 // Done be Rustam Zokirov (U1910049)
 // Last change done in April 13, 2020
@@ -11,23 +11,25 @@
 
 using namespace std;
 
-int main(); 
+int main();
 
-void F_First_Program() {
+void F_First_Program()
+{
 
 	// creating a text file one.txt
 	ofstream out_one;
 	out_one.open("one.txt");
-	for (int i = 2; i <= 2 * 10; i = i + 2) {
+	for (int i = 2; i <= 2 * 10; i = i + 2)
+	{
 		out_one << i << endl; // writing to file first ten even numbers
 	}
 	out_one.close(); // closing the file
 
-
 	// creating a text file one.txt
 	ofstream out_two;
 	out_two.open("two.txt");
-	for (int i = 5; i <= 5 * 10; i = i + 5) {
+	for (int i = 5; i <= 5 * 10; i = i + 5)
+	{
 		out_two << i << endl; // writing to file first ten multiples of five
 	}
 	out_two.close(); // closing the file
@@ -38,13 +40,13 @@ void F_First_Program() {
 	in_one.open("one.txt"); // opening files
 	in_two.open("two.txt");
 
-	
 	int total = 0;
 	int num1 = 0;
 	int num2 = 0;
 
-	while (in_one && in_two) {
-		total += num1 + num2; // calculating the total	
+	while (in_one && in_two)
+	{
+		total += num1 + num2; // calculating the total
 		in_one >> num1;
 		in_two >> num2;
 	}
@@ -65,17 +67,23 @@ void F_First_Program() {
 
 	in_total >> total;
 	cout << "TOTAL: " << total << "\n\n"; // displaying the total in console
-	
+
 	in_total.close(); // closing the file after executing
 }
 
+void F_Second_Program()
+{
 
-void F_Second_Program(){
-
-	for (int k = 0; k < 1000; k++) {
+	for (int k = 0; k < 1000; k++)
+	{
 		system("cls");
-		cout << "C O N T A C T S\n" << "------------------\n" << "1. Add a contact\n" << "2. Contacts\n" << "0. Back\n" << "Your choice: \n";
-		
+		cout << "C O N T A C T S\n"
+			 << "------------------\n"
+			 << "1. Add a contact\n"
+			 << "2. Contacts\n"
+			 << "0. Back\n"
+			 << "Your choice: \n";
+
 		ofstream out_contacts("contacts.txt", ios::app);
 
 		string name, phone;
@@ -83,82 +91,95 @@ void F_Second_Program(){
 		switch (_getch())
 		{
 			// case 49 is for adding a new contact into a list
-		case 49: {
+		case 49:
+		{
 			system("cls");
 			cout << "Adding a new contact. Input a contact info:\n\n";
 
-			cout << "Enter the name: "; cin >> name;
-			cout << "Enter the phone number: "; cin >> phone;
+			cout << "Enter the name: ";
+			cin >> name;
+			cout << "Enter the phone number: ";
+			cin >> phone;
 
 			// storing the data in file
-			out_contacts << left << setw (12) << name << "\t" << phone << "\n";
-			out_contacts.close(); //closing the file
+			out_contacts << left << setw(12) << name << "\t" << phone << "\n";
+			out_contacts.close(); // closing the file
 
 			cout << "Successfully added!\n\n";
-			
-		    system("pause");
+
+			system("pause");
 		}
-			break;
+		break;
 
-
-		case 50: {
+		case 50:
+		{
 			system("cls");
 			ifstream in_contacts("contacts.txt"); // getting data from the file
-			while (in_contacts >> name >> phone) {
+			while (in_contacts >> name >> phone)
+			{
 				// displaying the data
 				cout << left << setw(12) << name << "\t" << phone << endl;
 			}
 			in_contacts.close(); // closing the file
 			system("pause");
 		}
-			break;
+		break;
 
-
-		case 48: {
+		case 48:
+		{
 			main(); // to back to main menu
 		}
-			break;
+		break;
 
-		default: {
+		default:
+		{
 			cout << "Your choice is not available in Menu.\nPlease try one more time\n";
 			system("pause");
 		}
-			   break;
+		break;
 		} // switch
-	} // for loop
+	}	  // for loop
 }
 
+void F_Third_Program()
+{
 
-void F_Third_Program(){
-
-	for (int k = 0; k < 1000; k++) {
+	for (int k = 0; k < 1000; k++)
+	{
 		system("cls");
-		cout << "S E A R C H I N G  F O R  N U M B E R \n" << "------------------------------------\n" << "1. Add numbers\n" << "2. Search for number\n" << "0. Back\n" << "Your choice: \n";
+		cout << "S E A R C H I N G  F O R  N U M B E R \n"
+			 << "------------------------------------\n"
+			 << "1. Add numbers\n"
+			 << "2. Search for number\n"
+			 << "0. Back\n"
+			 << "Your choice: \n";
 
 		ofstream out_numbers("numbers.txt", ios::app); // the list could be contiunied after the program execution
 
 		int numbers;
-			
+
 		switch (_getch())
 		{
-		case 49: {
+		case 49:
+		{
 			system("cls");
 			cout << "ENYER NUMBERS\n";
 
 			// inputing numbers
-			for (int i = 1; i <= 20; i++) {
-				cout << "[ " << i << " ] -> "; cin >> numbers;
+			for (int i = 1; i <= 20; i++)
+			{
+				cout << "[ " << i << " ] -> ";
+				cin >> numbers;
 				out_numbers << numbers << endl;
 			}
-			out_numbers.close(); //closing the file
+			out_numbers.close(); // closing the file
 
-			
 			system("pause");
 		}
-			break;
+		break;
 
-
-		case 50: {
+		case 50:
+		{
 			system("cls");
 			ifstream in_numbers("numbers.txt");
 
@@ -166,10 +187,11 @@ void F_Third_Program(){
 
 			int search_number;
 			bool isAnswerHere = 0; // for finding the searching number from available list
-			cout << "Enter the number to search: "; 
+			cout << "Enter the number to search: ";
 			cin >> search_number;
 
-			while (in_numbers) {
+			while (in_numbers)
+			{
 				in_numbers >> numbers;
 				if (search_number == numbers)
 					isAnswerHere = 1;
@@ -182,28 +204,35 @@ void F_Third_Program(){
 
 			system("pause");
 		}
-			break;
+		break;
 
-
-		case 48: {
+		case 48:
+		{
 			main();
 		}
-			break;
+		break;
 
-		default: {
+		default:
+		{
 			cout << "Your choice is not available in Menu.\nPlease try one more time\n";
 			system("pause");
 		}
-			break;
+		break;
 		} // switch
-	} // for loop
+	}	  // for loop
 }
 
-
-int main(){
-	for (int k = 0; k < 1000; k++) {
+int main()
+{
+	for (int k = 0; k < 1000; k++)
+	{
 		system("cls");
-	    cout << "M A I N  M E N U\n"<< "-------------------\n" << "1. First Program\n" << "2. Second Program\n" << "3. Third Program\n" << "Your choice: \n";
+		cout << "M A I N  M E N U\n"
+			 << "-------------------\n"
+			 << "1. First Program\n"
+			 << "2. Second Program\n"
+			 << "3. Third Program\n"
+			 << "Your choice: \n";
 
 		switch (_getch())
 		{
@@ -213,13 +242,11 @@ int main(){
 			system("pause");
 			break;
 
-
 		case 50:
 			system("cls");
 			F_Second_Program();
 			system("pause");
 			break;
-
 
 		case 51:
 			system("cls");
@@ -227,11 +254,9 @@ int main(){
 			system("pause");
 			break;
 
-
 		case 48:
 			return 0;
 			break;
-
 
 		default:
 			cout << "Your choice is not available in Menu.\nPlease try one more time\n";
@@ -239,7 +264,7 @@ int main(){
 			break;
 
 		} // switch
-	} // for loop
+	}	  // for loop
 
 	system("pause");
 	return 0;
